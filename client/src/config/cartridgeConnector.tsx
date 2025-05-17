b5d2aa10585b3568e2908927103f6516384cce96
Marco Araya Jiménez <marcojime23@gmail.com>
2025-05-16 19:11:00 -0600

    FEAT: refactor Dojo configuration and add environment variable support

---
+++ client/src/config/cartridgeConnector.tsx
@@ -0,0 +1,47 @@
+
+import { Connector } from "@starknet-react/core";
+import { ControllerConnector } from "@cartridge/connector";
+import { ColorMode, SessionPolicies, ControllerOptions, } from "@cartridge/controller";
+import { constants } from "starknet";
+
+const { VITE_PUBLIC_DEPLOY_TYPE } = import.meta.env;
+
+const CONTRACT_ADDRESS_GAME = '0x7293e0d29ad517a34379faef457009e0325840fa6f7bd6b782a5473fa4663e3'
+
+const policies: SessionPolicies = {
+  contracts: {
+    [CONTRACT_ADDRESS_GAME]: {
+      methods: [
+        { name: "spawn_player", entrypoint: "spawn_player" },
+        { name: "reward_player", entrypoint: "reward_player" },
+        { name: "update_player_ranking", entrypoint: "update_player_ranking" },
+        { name: "unlock_golem_store", entrypoint: "unlock_golem_store" },
+        { name: "unlock_world_store", entrypoint: "unlock_world_store" },
+      ],
+    },
+  },
+}
+
+// Controller basic configuration
+const colorMode: ColorMode = "dark";
+const theme = "golem-runner";
+
+const options: ControllerOptions = {
+  chains: [
+    {
+      rpcUrl: "https://api.cartridge.gg/x/starknet/sepolia",
+    },
+  ],
+  defaultChainId: VITE_PUBLIC_DEPLOY_TYPE === 'mainnet' ?  constants.StarknetChainId.SN_MAIN : constants.StarknetChainId.SN_SEPOLIA,
+  policies,
+  theme,
+  colorMode,
+  namespace: "golem_runner", 
+  slot: "golem_runner_achievements", 
+};
+
+const cartridgeConnector = new ControllerConnector(
+  options,
+) as never as Connector;
+
+export default cartridgeConnector;
