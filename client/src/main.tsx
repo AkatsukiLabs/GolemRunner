79997235c4cd3db8d6373432a48942423e3efac8
Marco Araya Jiménez <marcojime23@gmail.com>
2025-05-17 19:02:11 -0600

    feat: add service worker and manifest for PWA support

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,26 @@
-import React from "react"
-import { createRoot } from "react-dom/client"
-import App from "./app/App"
-import "./global.css"                       
-import "slick-carousel/slick/slick.css"     
-import "slick-carousel/slick/slick-theme.css"
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app/App";
+import "./global.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
+
+// Service Worker registration
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered succesfully:', registration);
+      })
+      .catch(error => {
+        console.log('Error registering the Service Worker:', error);
+      });
+  });
+}
+
