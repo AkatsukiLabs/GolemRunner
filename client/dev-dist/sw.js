9bab64a9b591f72a66578ecc20b1c0984c7f0a3f
Marco Araya Jiménez <marcojime23@gmail.com>
2025-05-20 21:01:02 -0600

    feat: improve map assets

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.falqb6v11kg"
+    "revision": "0.lg1ai95qqtg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
